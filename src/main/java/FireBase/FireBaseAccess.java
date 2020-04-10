358b62c5d465835accbb06eaa9773493f4d6f458
Maxence <maxence.rouget@gmail.com>
2020-04-10 13:56:34 +0200

    Firbase works, need FaceRegnition

--- src/main/java/FireBase/FireBaseAccess.java
+++ src/main/java/FireBase/FireBaseAccess.java
@@ -9,6 +9,7 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import com.google.firebase.database.*;
 
+import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,7 +19,6 @@ import java.util.concurrent.ExecutionException;
 
 public class FireBaseAccess {
     public Firestore db;
-
     public FireBaseAccess() throws IOException {
         FileInputStream serviceAccount =
                 new FileInputStream("./gosecuriepsi-firebase-adminsdk-gc6mg-c0c10802bc.json");
@@ -32,23 +32,19 @@ public class FireBaseAccess {
         this.db = FirestoreClient.getFirestore();
     }
 
-
-
  public boolean GetDataFromDatabase() throws ExecutionException, InterruptedException {
      CollectionReference users = db.collection("Users");
      Query query = users.orderBy("name");
      ApiFuture<QuerySnapshot> querySnapshot = query.get();
      List<DocumentSnapshot> documents = new ArrayList<DocumentSnapshot>();
 
-
      for(DocumentSnapshot document : querySnapshot.get().getDocuments()){
          documents.add(document);
      }
-
      for(DocumentSnapshot doc : documents){
          System.out.println(doc.getData());
      }
 
      return true;
+    }
  }
-}
--- src/main/java/Gui/Camera.java
+++ src/main/java/Gui/Camera.java
@@ -1,10 +1,15 @@
 package Gui;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
+import org.opencv.face.EigenFaceRecognizer;
+import org.opencv.face.FaceRecognizer;
+import org.opencv.face.FisherFaceRecognizer;
 import org.opencv.videoio.VideoCapture;
 import org.opencv.imgcodecs.Imgcodecs;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
@@ -13,11 +18,32 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static javax.imageio.ImageIO.read;
 
 public class Camera extends JFrame {
+    static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+
+    private JPanel MainPanel;
+    private JButton startButtonTEST;
+    private JButton button2;
+    private JPanel CameraPanel;
+
+    private DaemonThread myThread = null;
+    int count = 0;
+    VideoCapture webSource = null;
+
+    Mat frame = new Mat();
+    MatOfByte mem = new MatOfByte();
 
     class DaemonThread implements Runnable{
         protected volatile boolean runnable = false;
@@ -59,21 +85,9 @@ public class Camera extends JFrame {
             }
         }
     }
-    public JPanel MainPanel;
-    private JButton startButtonTEST;
-    private JButton button2;
-    private JPanel CameraPanel;
-
-    private DaemonThread myThread = null;
-    int count = 0;
-    VideoCapture webSource = null;
-
-    Mat frame = new Mat();
-    MatOfByte mem = new MatOfByte();
-
 
     public Camera() {
-        startButtonTEST.addMouseListener(new MouseAdapter() {
+        /*startButtonTEST.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
@@ -86,13 +100,23 @@ public class Camera extends JFrame {
                 startButtonTEST.setEnabled(false);  //start button
                 button2.setEnabled(true);  // stop button
             }
-        });
+        });*/
+
+        //Run on lunch
+        webSource =new VideoCapture(0);
+        myThread = new DaemonThread();
+        Thread t = new Thread(myThread);
+        t.setDaemon(true);
+        myThread.runnable = true;
+        t.start();
+        startButtonTEST.setEnabled(false);  //start button
+        button2.setEnabled(true);  // stop button
 
         button2.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                File file = new File("./test.jpg");
+                File file = new File("/Users/maxencerouget/Desktop/OpencvPython/img/Unknow/What.jpg");
                 Imgcodecs.imwrite(file.getPath(), frame);
 
                 myThread.runnable = false;
@@ -100,14 +124,51 @@ public class Camera extends JFrame {
                 startButtonTEST.setEnabled(true);
 
                 try {
-                    var test = encodeFileToBase64Binary(file);
-                    System.out.println();
-                    System.out.println(test);
+                    //var test = encodeFileToBase64Binary(file);
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
+                myThread.runnable = true;
+                button2.setEnabled(true);
+
             }
         });
+    }
+
+    public static List<String> ListFile(String pathToFolder){
+        List<String> filesKnow = new ArrayList<String>();
+
+        try (Stream<Path> walk = Files.walk(Paths.get(pathToFolder))) {
+
+            filesKnow = walk.filter(Files::isRegularFile)
+                    .map(x -> x.toString()).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return filesKnow;
+    }
+
+
+    public static void Reconizer(){
+        List<String> filesKnow = ListFile("./img/Know");
+        List<String> filesUnKnow = ListFile("./img/Unknow");
+        List<Mat> MatUnKnow = new ArrayList<Mat>();
+        Mat fileKnow = new Mat();
+        Mat fileToCompare = new Mat();
+
+        for(var path : filesKnow){
+            fileKnow = Imgcodecs.imread(path);
+        }
+        for(var path : filesUnKnow){
+            Mat matTemp = Imgcodecs.imread(path);
+            MatUnKnow.add(matTemp);
+        }
+
+        EigenFaceRecognizer model = EigenFaceRecognizer.create();
+        model.train(MatUnKnow, fileKnow);
+        fileToCompare = Imgcodecs.imread("./img/Unknow/What.jpg");
+        int predict = model.predict_label(fileToCompare);
+        var test = "testy";
     }
 
     private static String encodeFileToBase64Binary(File file) throws Exception{
@@ -115,5 +176,23 @@ public class Camera extends JFrame {
         byte[] bytes = new byte[(int)file.length()];
         fileInputStreamReader.read(bytes);
         return new String(Base64.getEncoder().encode(bytes),"UTF-8");
+    }
+    public static void decodeFileFromBase64Binary(String fileStringed) throws IOException {
+        byte[] bar = Base64.getDecoder().decode(fileStringed.getBytes());
+        ByteArrayInputStream bis = new ByteArrayInputStream(bar);
+        BufferedImage bImage2 = ImageIO.read(bis);
+        ImageIO.write(bImage2, "jpg", new File("./output.jpg") );
+        System.out.println("image created");
+
+    }
+
+    public static void main(String[] args) {
+/*        JFrame frame = new JFrame("GoSecuriApp");
+        frame.setContentPane(new Camera().MainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(800, 800));
+        frame.pack();
+        frame.setVisible(true);*/
+        Reconizer();
     }
 }
